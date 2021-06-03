5428992963e49f2106bb9538cb26a7db0299cd40
Muhammad Kuzibaev <K.Muhammad@pixel.comp>
2021-06-03 18:10:17 +0500

    upload files

---
+++ components/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('DATABASE', 'todolist');
+define('DB_PREFIX', 'bje_');
+
+define('LAYOUT', 'layout.php');
+define('PAGINATION', 3);
